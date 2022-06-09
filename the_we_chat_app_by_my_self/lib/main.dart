aecf4a3f1e7d45d805bf4fca2f8d484e3db24e5a
PyiTheimKyaw <92578990+PyiTheimKyaw@users.noreply.github.com>
2022-06-09 20:36:19 +0630

    import firebase dependencies

--- the_we_chat_app_by_my_self/lib/main.dart
+++ the_we_chat_app_by_my_self/lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:the_we_chat_app_by_my_self/pages/start_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
